6d21a767c563c8e1da5a8b1a3dd0432610ec6426
Jérémy De Sousa <jeremy.de-sousa@etu.univ-lyon1.fr>
2015-09-15 22:11:16 +0200

    add hibernate

---
+++ ListePays/src/database/metier/PaysHome.java
@@ -0,0 +1,119 @@
+package database.metier;
+// Generated 15 sept. 2015 22:10:17 by Hibernate Tools 4.3.1
+
+import java.util.List;
+
+import javax.naming.InitialContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Pays.
+ * 
+ * @see metier.Pays
+ * @author Hibernate Tools
+ */
+public class PaysHome {
+
+	private static final Log log = LogFactory.getLog(PaysHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Pays transientInstance) {
+		log.debug("persisting Pays instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Pays instance) {
+		log.debug("attaching dirty Pays instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Pays instance) {
+		log.debug("attaching clean Pays instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Pays persistentInstance) {
+		log.debug("deleting Pays instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Pays merge(Pays detachedInstance) {
+		log.debug("merging Pays instance");
+		try {
+			Pays result = (Pays) sessionFactory.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Pays findById(java.lang.String id) {
+		log.debug("getting Pays instance with id: " + id);
+		try {
+			Pays instance = (Pays) sessionFactory.getCurrentSession().get("metier.Pays", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Pays instance) {
+		log.debug("finding Pays instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("metier.Pays")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
