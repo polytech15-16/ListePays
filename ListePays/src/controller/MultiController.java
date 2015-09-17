fa494de4197527c46060bea27c43d778483d69dd
Jérémy De Sousa <jeremy.de-sousa@etu.univ-lyon1.fr>
2015-09-17 15:14:11 +0200

    Project in progress

--- ListePays/src/controller/MultiController.java
+++ ListePays/src/controller/MultiController.java
@@ -10,11 +10,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
+import requeteSOAP.EnvoiMessage;
+
 @Controller
 public class MultiController extends MultiActionController {
 
+	private static String lister_pays = "getPays";
+	private static String capitale = "getCapitale";
+	private static String habitants = "getNombreHab";
+	private static String destenvoi = "http://localhost:8080/ProjetWSPays/services/WSPays";
+	private static String destination = "http://pays";
+
+	private static EnvoiMessage unAppel = new EnvoiMessage();
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView hello(@RequestParam(required = false) String pays) {
+
+		// On récupère la liste des pays
+		unAppel.connexion();
+		unAppel.creationMessage(lister_pays, destination, "");
+		unAppel.EmmissionReception(destenvoi, "");
+
 		ModelAndView model = new ModelAndView();
 		model.setViewName("hello");
 
---
+++ ListePays/src/requeteSOAP/EnvoiMessage.java
@@ -0,0 +1,114 @@
+package requeteSOAP;
+
+import java.util.Iterator;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPConnection;
+import javax.xml.soap.SOAPConnectionFactory;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Node;
+
+public class EnvoiMessage {
+	private SOAPConnection connection;
+	private SOAPConnectionFactory soapConnFactory;
+	private MessageFactory messageFactory;
+	private SOAPMessage message;
+	private SOAPPart soapPart;
+	private SOAPEnvelope envelope;
+	private SOAPBody body;
+	private SOAPElement bodyElement;
+	private TransformerFactory transformerFactory;
+	private Transformer transformer;
+	private Source sourceContent;
+
+	// fonction connexion
+
+	// on construit une connexion
+	public void connexion() {
+		try {
+			soapConnFactory = SOAPConnectionFactory.newInstance();
+			connection = soapConnFactory.createConnection();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	// ** Création de l’objet message
+	// On construit les différentes parties du message SOAP
+	// Il est possible de créer le message à partir d’un fichier externe.**/
+	public void creationMessage(String operation, String destination, String pays) {
+		try {
+			messageFactory = MessageFactory.newInstance();
+			message = messageFactory.createMessage();
+			soapPart = message.getSOAPPart();
+			envelope = soapPart.getEnvelope();
+			body = envelope.getBody();
+			// On crée l'élément principal et le namespace'
+			QName bodyName = new QName(destination, operation, "m");
+			// On crée l’enveloppe
+			bodyElement = body.addBodyElement(bodyName);
+			// On passe les paramêtres
+			if (pays != "") {
+				QName qn1 = new QName("pays");
+				bodyElement.addChildElement(qn1).addTextNode(pays);
+			}
+			// On sauve le message
+			message.saveChanges();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void EmmissionReception(String destination, String pays)
+
+	{
+		try {
+			// On contrôle l'entrée
+			System.out.println("\nENVOI:\n");
+			message.writeTo(System.out);
+			System.out.println();
+			// On envoie le message et on attend la réponse
+			// On définit la destination
+			// On envoie le message
+			SOAPMessage reply = connection.call(message, destination);
+
+			// traitement de la réponse
+			// On contrôle la sortie
+			System.out.println("\nREQUEST:\n");
+			soapPart = reply.getSOAPPart();
+			envelope = soapPart.getEnvelope();
+			body = envelope.getBody();
+			// on examine les éléments renvoyés dans une liste
+			Iterator iter = body.getChildElements();
+			Node resultOuter = ((Node) iter.next()).getFirstChild();
+			Node result = resultOuter.getFirstChild();
+			// on affiche le résultat
+			System.out.println(result.getNodeValue());
+
+			// on crée le transformeur pour visualiser le message
+			transformerFactory = TransformerFactory.newInstance();
+			transformer = transformerFactory.newTransformer();
+			// On extrait le contenu du corps BODY
+			sourceContent = reply.getSOAPPart().getContent();
+			// Sortie de la transformation
+			StreamResult unresult = new StreamResult(System.out);
+			transformer.transform(sourceContent, unresult);
+			System.out.println();
+			// on ferme la connexion
+			connection.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
